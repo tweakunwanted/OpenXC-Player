32fe888a19244d95c422110b19c3ac573d334be4
tweakunwanted <56216907+tweakunwanted@users.noreply.github.com>
2019-10-07 00:54:02 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt install apache2 php -y
+curl -s https://raw.githubusercontent.com/tweakunwanted/OpenXC-Player/master/php7.2-ext.sh | sudo bash
+rm -rf /var/www/html
+git clone https://github.com/tweakunwanted/OpenXC-Player /var/www/html
+rm -rf !$/.git
+rm -rf /var/www/html/.gitattributes
