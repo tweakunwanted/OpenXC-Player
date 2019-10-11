2d262786fd64301f076c72d51b78998da2dbc293
tweakunwanted <56216907+tweakunwanted@users.noreply.github.com>
2019-10-10 21:21:20 -0300

    restart apache2 after done

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt install apache2 php7.2 -y
+apt install apache2 php7.2 wget curl git -y
 curl -s https://raw.githubusercontent.com/tweakunwanted/OpenXC-Player/master/php7.2-ext.sh | sudo bash
 rm -rf /var/www/html
 git clone https://github.com/tweakunwanted/OpenXC-Player /var/www/html
@@ -8,3 +8,4 @@ rm -rf /var/www/html/install.sh
 rm -rf /var/www/html/README.md
 rm -rf /var/www/html/php7.2-ext.sh
 wget -q https://raw.githubusercontent.com/tweakunwanted/OpenXC-Player/master/000-default.conf -O /etc/apache2/sites-available/000-default.conf
+service apache2 restart
