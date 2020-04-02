f0457faf78877933727de786c7d77cbb2ef3ec8e
R3tr0R4z0r <11089936+R3tr0R4z0r@users.noreply.github.com>
2020-04-02 12:19:52 +0100

    Create install-ENG.sh

---
+++ install-ENG.sh
@@ -0,0 +1,12 @@
+  
+apt update && apt install apache2 php7.2 wget curl git -y
+curl -s https://raw.githubusercontent.com/R3tr0R4z0r/OpenXC-Player/master/php7.2-ext.sh | sudo bash
+rm -rf /var/www/html
+git clone https://github.com/R3tr0R4z0r/OpenXC-Player /var/www/html
+rm -rf !$/.git
+rm -rf /var/www/html/.gitattributes
+rm -rf /var/www/html/install.sh
+rm -rf /var/www/html/README.md
+rm -rf /var/www/html/php7.2-ext.sh
+wget -q https://raw.githubusercontent.com/R3tr0R4z0r/OpenXC-Player/master/000-default.conf -O /etc/apache2/sites-available/000-default.conf
+service apache2 restart
