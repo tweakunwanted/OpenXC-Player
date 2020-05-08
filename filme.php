1881dbc41312622c7ef653a52df8883256318f2b
R3tr0R4z0r <11089936+R3tr0R4z0r@users.noreply.github.com>
2020-05-08 21:32:23 +0100

    Update filme.php

--- filme.php
+++ filme.php
@@ -98,7 +98,7 @@ $duracao = $output['info']['duration'];
                         <!-- 16:9 aspect ratio -->
         <div class="embed-responsive embed-responsive-16by9 video-embed-box">
         
-        <?php if($exts == 'avi' || $exts == 'mkv') { ?>
+        <?php if($exts == 'xxx') { ?>
         <center><br><br>
         <h4><?php echo ERROR_FORMATO_VIDEO; ?>: <?php echo $exts; ?></h4>
         <br><br>
